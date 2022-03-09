f5b4db5cccc43fc10c6f7e29fb0a26fc9eb17f74
Paige Steffan <ryd429@vols.utk.edu>
2022-03-08 20:29:38 -0500

    Dashboard Feature 1
    Final code for Feature 1- including dashboard

---
+++ Dashboard- Feature1.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(quantmod)
+library(shinydashboard)
+
+
+SYMBOLS <- stockSymbols()
+
+library(shiny)
+
+ui <- dashboardPage(
+  dashboardHeader(title= "Stock Dashboard"), 
+  dashboardSidebar(), 
+  dashboardBody(
+  box(title= "Company Selection", status= "primary" ,  align= "center", height= 250,
+      selectInput("select",
+              label = h3("Select A Stock Name"), 
+              choices = names(table(SYMBOLS$Name)), 
+              selected = 1),
+  hr(),
+  fluidRow(column(3, verbatimTextOutput("value")))
+)))
+
+server <- function(input, output, session) {
+  output$value <- renderPrint({ SYMBOLS$Symbol[which(SYMBOLS$Name == input$select)] })
+}
+
+shinyApp(ui, server)
+
